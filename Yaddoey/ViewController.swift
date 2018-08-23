04c855815f3fe6ecb87b25c46a04f893d1d6103f
Khalid SH <khalid@khalids-macbook-pro.local>
2018-08-23 13:26:51 +0300

    Initial Commit

---
+++ Yaddoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yaddoey
+//
+//  Created by Khalid SH on 23/08/2018.
+//  Copyright © 2018 Khalid SH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ YaddoeyTests/YaddoeyTests.swift
@@ -0,0 +1,34 @@
+//
+//  YaddoeyTests.swift
+//  YaddoeyTests
+//
+//  Created by Khalid SH on 23/08/2018.
+//  Copyright © 2018 Khalid SH. All rights reserved.
+//
+
+import XCTest
+@testable import Yaddoey
+
+class YaddoeyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
